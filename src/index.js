c61ece878d13373be76bde11324c3f38af226918
Shun Yao <ysmiracle@hotmail.com>
2019-01-29 16:09:24 -0500

    starting adding redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//import { createStore } from 'redux';
-//import { Provider } from 'react-redux';
-//import reducer from './reducers';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import 'semantic-ui-css/semantic.min.css';
 
-//const store = createStore(reducer);
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
 
-ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
