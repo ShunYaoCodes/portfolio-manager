4c2422459bf3edb525294b20f9ba7f281285913b
Shun Yao <shunyaocodes@gmail.com>
2019-10-29 22:27:27 -0400

    use compose to fix 'passing several store enhancers to createStore()' bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default createStore(
     rootReducer,
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancer(applyMiddleware(thunk)),
 );
