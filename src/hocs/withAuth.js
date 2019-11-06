b28c7d685b704dbe4e460de6dce0217ddbee5f05
Shun Yao <shunyaocodes@gmail.com>
2019-11-05 22:02:08 -0500

    replace AuthAdapter with redux state auth in withAuth hoc class

--- src/hocs/withAuth.js
+++ src/hocs/withAuth.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import AuthAdapter from '../adapters/AuthAdapter';
+import { connect } from 'react-redux';
 
 function withAuth(WrappedComponent) {
-  return class extends React.Component {
+  class Wrapper extends React.Component {
     render() {
-      return AuthAdapter.loggedIn() ? <Redirect to='/' /> : <WrappedComponent {...this.props} />
+      return this.props.token ? <Redirect to='/' /> : <WrappedComponent {...this.props} />
     }
   }
+
+  const mapStateToProps = (state) => {
+    const { token } = state.auth;
+    return { token };
+  };
+  
+  return connect(mapStateToProps)(Wrapper);
 }
 
 export default withAuth;
