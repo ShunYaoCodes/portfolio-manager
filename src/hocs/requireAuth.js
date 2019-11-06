1bf0475035a9f4ad7a560b10be86180e37362c71
ShunYaoCodes <shunyaocodes@gmail.com>
2019-11-06 16:04:36 -0500

    replace AuthAdapter with redux state auth in requireAuth hoc class

--- src/hocs/requireAuth.js
+++ src/hocs/requireAuth.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import AuthAdapter from '../adapters/AuthAdapter';
+import { connect } from 'react-redux';
 
 function requireAuth(WrappedComponent) {
-  return class extends React.Component {
+  class Wrapper extends React.Component {
     render() {
-      return AuthAdapter.notLoggedIn() ? <Redirect to='/login' /> : <WrappedComponent {...this.props} />;
+      return this.props.token ? <WrappedComponent {...this.props} /> : <Redirect to='/login' />;
     }
   }
+
+  const mapStateToProps = (state) => {
+    const { token } = state.auth;
+    return { token };
+  };
+
+  return connect(mapStateToProps)(Wrapper);
 }
 
 export default requireAuth;
