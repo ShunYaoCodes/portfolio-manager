d6a1e29cf1520193dcf30ef8ec2d07da8734149e
Shun Yao <shunyaocodes@gmail.com>
2019-11-16 22:43:11 -0500

    refactor Index, add css to make index grids more mobile friendly

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
-import { Grid, Icon } from 'semantic-ui-react'
+import { Grid, Icon, Segment } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 import { fetchStockDetail } from '../redux/actions';
 
@@ -32,54 +32,27 @@ const Index = props => {
     props.dispatch(fetchStockDetail(event.target.name));
   };
 
-  const vix = <React.Fragment>
-              {props.change >= 0 ?
-              <Grid.Column textAlign='right' color='green' width={2}>
-                <NavLink style={{color: 'white'}} to={link} name={props.symbol} onClick={handleClick}>
-                  <p>{symbol}</p>
-                  <p>{props.latestPrice}&nbsp;&nbsp;+{props.change}</p>
-                  <p><Icon name='triangle up' />&nbsp;(+{(props.changePercent*100).toFixed(2)}%)</p>
-                </NavLink>
-              </Grid.Column>
-            :
-              <Grid.Column textAlign='right' color='red' width={2}>
-                <NavLink style={{color: 'white'}} to={link} name={props.symbol} onClick={handleClick}>
-                  <p>{symbol}</p>
-                  <p>{props.latestPrice}&nbsp;&nbsp;{props.change}</p>
-                  <p><Icon name='triangle down' />&nbsp;({(props.changePercent*100).toFixed(2)}%)</p>
-                </NavLink>
-              </Grid.Column>
-          }
-          </React.Fragment>
-
-    const indexes = <React.Fragment>
-            {props.change >= 0 ?
-                <Grid.Column textAlign='right' color='green' width={2}>
-                  <NavLink style={{color: 'white'}} to={link} name={props.symbol} onClick={handleClick}>
-                    <p>{symbol}</p>
-                    <p>{props.latestPrice}&nbsp;&nbsp;+{props.change}</p>
-                    <p><Icon name='triangle up' />&nbsp;(+{(props.changePercent*100).toFixed(2)}%)</p>
-                  </NavLink>
-                </Grid.Column>
-              :
-                <Grid.Column textAlign='right' color='red' width={2}>
-                  <NavLink style={{color: 'white'}} to={link} name={props.symbol} onClick={handleClick}>
-                    <p>{symbol}</p>
-                    <p>{props.latestPrice}&nbsp;&nbsp;{props.change}</p>
-                    <p><Icon name='triangle down' />&nbsp;({(props.changePercent*100).toFixed(2)}%)</p>
-                  </NavLink>
-                </Grid.Column>
-              }
-                <Grid.Column width={1}>
-                </Grid.Column>
-            </React.Fragment>
-
-
+  const indexes = props.change >= 0 ?
+    <Segment inverted color={symbol === 'VIX' ? 'red' : 'green'}>
+      <NavLink style={{color: 'white'}} to={link} name={props.symbol} onClick={handleClick}>
+        <p>{symbol}</p>
+        <p>{props.latestPrice}&nbsp;&nbsp;+{props.change}</p>
+        <p><Icon className='mobile-hide' name='triangle up' />&nbsp;(+{(props.changePercent*100).toFixed(2)}%)</p>
+      </NavLink>
+    </Segment>  
+    :
+    <Segment inverted color={symbol === 'VIX' ? 'green' : 'red'}>  
+      <NavLink style={{color: 'white'}} to={link} name={props.symbol} onClick={handleClick}>
+        <p>{symbol}</p>
+        <p>{props.latestPrice}&nbsp;&nbsp;{props.change}</p>
+        <p><Icon name='triangle down' />&nbsp;({(props.changePercent*100).toFixed(2)}%)</p>
+      </NavLink>
+    </Segment>    
 
   return (
-    <React.Fragment>
-      {symbol === 'VIX' ? vix : indexes}
-    </React.Fragment>
+    <Grid.Column textAlign='right' computer={2} mobile={3}>
+      {indexes}
+    </Grid.Column>
   )
 }
 
